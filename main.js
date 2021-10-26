8119d58e6a7110fa6661e3dfbb564987a62b7300
martin-arg <martin.gigli@gmail.com>
2021-10-25 22:57:51 -0300

    v 1
    ya esta

--- main.js
+++ main.js
@@ -3,40 +3,21 @@ let totalApagar = document.getElementById('buttontotalApagar');
 
 totalApagar.addEventListener('click', (e)=> {
     e.preventDefault();
- 
-    totalApagar();
-});
-
-
-
-function totalApagar() {
-    let cantida =  document.getElementById("cantidad").value;
-    let cate = document.getElementById("categoria");
-    let categorias = cate.options[cate.selectedIndex].text;
+    let cantida =  parseInt(document.getElementById("cantidad").value);
+    let categorias = document.getElementById('categoria');
+    let categori = categorias.options[categorias.selectedIndex].value;
     let valor= 0; //cambiar
-    let numero= parseInt(cantida); //
     
-    document.getElementById("total").innerHTML= "Total a Pagar: $" + valor;
-alert("esto corre");
-/*    
-    if ( isNaN(numero)){
-        alert("EN CANTIDAD INGRESE SOLO NUMEROS ENTEROS");
-    }else{
-        switch (categorias) {
-               case "Estudiante":
-                    valor= parseInt(cantida)*200*0.2;
-                    document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
-                    break;
-              case "Trainee":
-                   valor = parseInt(cantida)*200*0.5; 
-                   document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
-                   break;
-             case "Junior":
-                   valor = parseInt(cantida)*200*0.85; 
-                   document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
-                   break;
-             default:
-                   break;
-        }
-   }*/
-}
+    //document.getElementById("total").innerHTML= "Total a Pagar: $" + cantida;
+    //alert(categori)
+    if (isNaN(cantida)) {
+        document.getElementById("total").innerHTML= "Ingrese un numero en cantidad"; 
+        
+    } else if (categori === "Estudiante") {
+        document.getElementById("total").innerHTML= "Total a Pagar: $" + cantida * 80;
+    } else if (categori === "Trainee") {
+        document.getElementById("total").innerHTML= "Total a Pagar: $" + cantida * 100;
+    } else if (categori === "Junior") {
+        document.getElementById("total").innerHTML= "Total a Pagar: $" + cantida * 170;
+    }
+});
