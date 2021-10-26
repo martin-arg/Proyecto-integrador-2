1e4b6d92015d83e3d0a965a04db974d1ddb7f209
martin-arg <martin.gigli@gmail.com>
2021-10-25 21:47:29 -0300

    v 0.6
    desarrollando la funcion

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-let titulo = document.getElementsByTagName("h1").inner;
-titulo.array.forEach(element => {
-    
+let totalApagar = document.getElementById('buttontotalApagar');
+
+
+totalApagar.addEventListener('click', (e)=> {
+    e.preventDefault();
+ 
+    totalApagar();
 });
-console.log(titulo);
+
+
+
+function totalApagar() {
+    let cantida =  document.getElementById("cantidad").value;
+    let cate = document.getElementById("categoria");
+    let categorias = cate.options[cate.selectedIndex].text;
+    let valor= 0; //cambiar
+    let numero= parseInt(cantida); //
+    
+    document.getElementById("total").innerHTML= "Total a Pagar: $" + valor;
+alert("esto corre");
+/*    
+    if ( isNaN(numero)){
+        alert("EN CANTIDAD INGRESE SOLO NUMEROS ENTEROS");
+    }else{
+        switch (categorias) {
+               case "Estudiante":
+                    valor= parseInt(cantida)*200*0.2;
+                    document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
+                    break;
+              case "Trainee":
+                   valor = parseInt(cantida)*200*0.5; 
+                   document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
+                   break;
+             case "Junior":
+                   valor = parseInt(cantida)*200*0.85; 
+                   document.getElementById("ptotal").innerHTML="Total a Pagar: $"+valor;
+                   break;
+             default:
+                   break;
+        }
+   }*/
+}
